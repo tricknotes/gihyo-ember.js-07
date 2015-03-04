899a269f6db6e2a05a275e37f47044b6d8437333
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-03-05 00:28:58 +0900

    Render response as 'json' by default

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments, only: %i(create update)
 
-  namespace :v1 do
-    resources :posts, only: %i(index show)
-  end
+  with_options defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts, only: %i(index show)
+    end
 
-  namespace :v2 do
-    resources :posts, only: %i(index show)
+    namespace :v2 do
+      resources :posts, only: %i(index show)
+    end
   end
 end
